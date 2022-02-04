27a1a2cc1e2e2ad5b1107b6df6ffd5ade1b80c7a
Eunyeoul Lee <yiio08@naver.com>
2022-02-05 00:46:24 +0900

    comment added

--- Self-study/2587.py
+++ Self-study/2587.py
@@ -1,14 +1,11 @@
 import sys
-num = [int(sys.stdin.readline().strip()) for _ in range(0,5)]
+num = [int(sys.stdin.readline().strip()) for _ in range(0,5)] #5개까지 입력
 
-num.sort()
+num.sort() #오름차순으로 정렬
 
 avg = 0
 
-for i in range (0,5):
+for i in range (0,len(num)):
     avg += num[i]
-print(int(avg/len(num)), end='\n')
+print(int(avg/len(num)), end='\n') #int로 안 바꿔주면 '.0'이 같이 출력됨
 print(num[2])
-
-
-
