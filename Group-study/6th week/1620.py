19289a07f577322d482920820e5a49a83af4027b
Eunyeoul Lee <yiio08@naver.com>
2022-03-04 13:27:10 +0900

    addtional explanation

--- Group-study/6th week/1620.py
+++ Group-study/6th week/1620.py
@@ -10,14 +10,14 @@ pokemon_name = {} # {포켓몬이름 : 포켓몬넘버} 딕셔너리 생성
 pokemon_num = {}  # {포켓몬넘버 : 포켓몬이름} 딕셔너리 생성
 
 for i in range(1, N+1): # 포켓몬 개수만큼 반복
-    name = input().rstrip()
+    name = input().rstrip() # 포켓몬 이름 입력 받기
     pokemon_num[i] = name # {1: name1 , 2: name2 ...}
     pokemon_name[name] = i # {name1: 1, name2: 2 ...}
 
 for _ in range(0,M): # 문제 개수만큼 반복
-    q = input().rstrip()
+    q = input().rstrip() # 문제 입력 받기
 
-    if q.isdigit(): # 숫자인지 판별
+    if q.isdigit(): # 문제가 숫자인지 판별
         print(pokemon_num[int(q)]) # q가 숫자면, pokemon_num에서 q를 찾아 해당 값을 출력
     else:
         print(pokemon_name[q]) # q가 숫자가 아닐 경우, pokemon_name에서 q를 찾아 해당 값을 출력
